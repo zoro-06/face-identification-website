7f55562c4b819b032b0cc9c8323377ecfadb6c08
j sandeep <136688470+zoro-06@users.noreply.github.com>
2024-03-12 19:40:39 +0530

    done

--- src/components/button/CaptureBtn.jsx
+++
@@ -1,9 +0,0 @@
-import styles from "./button.module.css"
-
-const CaptureBtn = () => {
-    return (
-        <div className={styles.captureBtn}>Capture</div>
-    )
-}
-
-export default CaptureBtn;
--- src/components/button/DeleteBtn.jsx
+++
@@ -1,9 +0,0 @@
-import styles from "./button.module.css"
-
-const DeleteBtn = () => {
-    return (
-        <div className={styles.deleteBtn}>Delete</div>
-    )
-}
-
-export default DeleteBtn;
